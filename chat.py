1d730f223191455b1476951e14d0284103fdec38
clark1816 <33646776+clark1816@users.noreply.github.com>
2022-11-15 09:37:30 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,9 +1,7 @@
 import openai
 from api_key import API_KEY
 import streamlit as st
-import sounddevice as sd
-import wavio
-import whisper
+
 
 openai.api_key = st.secrets["API_KEY"]
 
@@ -46,21 +44,7 @@ if option == 'Chat Helper Bot Ai':
         st.write('AI:',response)
         conversation.append('AI: %s' % response)
         
-def recrod_function():
-    with st.empty():
-        if record == True:
-            st.write("Recording...")
-            duration = 5  # seconds
-            fs = 48000
-            sd.default.samplerate = fs
-            sd.default.channels = 1
-            myrecording = sd.rec(int(duration * fs))
-            sd.wait(duration)
-            print("Saving sample as myvoice.mp3")
-            path_myrecording = "myvoice.mp3"
-            wavio.write(path_myrecording, myrecording, fs, sampwidth=2)
-            sd.play(myrecording, fs) #st
-            st.write("Done! Saved sample as myvoice.mp3")
+
 
             
 
