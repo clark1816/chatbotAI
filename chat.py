2957e806c12de077709f25dbde0ff796f2102b7e
clark1816 <33646776+clark1816@users.noreply.github.com>
2022-12-07 13:24:43 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -10,12 +10,47 @@ import streamlit.components.v1 as components
 
 openai.api_key = st.secrets["API_KEY"]
 
-option = st.sidebar.selectbox("Which Dashboard?", ('Home','Chat Helper Bot Ai','AI Translator'),0)
+option = st.sidebar.selectbox("Which Dashboard?", ('Home','Chat Helper Bot Ai','Jesus Ai','AI Translator'),0)
 if option == 'Home':
     st.header(option)
     st.write('The current home for all things AI since other website has decided to shit the bed.')
 
 if option == 'Chat Helper Bot Ai':
+    st.header(option)
+    def open_file(filepath):
+        with open(filepath, 'r', encoding='utf-8') as infile:
+            return infile.read()
+
+
+    def gpt3_completion(prompt, engine='text-davinci-002', temp=.9, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['AI:', 'USER:']):
+        prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
+        response = openai.Completion.create(
+            engine=engine,
+            prompt=prompt,
+            temperature=temp,
+            max_tokens=tokens,
+            top_p=top_p,
+            frequency_penalty=freq_pen,
+            presence_penalty=pres_pen,
+            stop=stop)
+        text = response['choices'][0]['text'].strip()
+        return text
+
+
+    if __name__ == '__main__':
+        conversation = list()
+        user_input = st.text_input(label='input') #input('USER: ')
+        conversation.append('USER: %s' % user_input)
+        #this line is basically a list comprehesion
+        text_block = '\n'.join(conversation)
+        prompt = open_file('jesus_prompt_chat.txt').replace('<<BLOCK>>', text_block)
+        prompt = prompt + '\n Jesus:'
+        response = gpt3_completion(prompt)
+        st.write('AI:',response)
+        st.image(response)
+        conversation.append('AI: %s' % response)
+        
+if option == 'Jesus Ai':
     st.header(option)
     def open_file(filepath):
         with open(filepath, 'r', encoding='utf-8') as infile:
