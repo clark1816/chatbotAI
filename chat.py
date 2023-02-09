fbbac1cfb738379c8a462300f0fa59ad24f908d3
clark1816 <33646776+clark1816@users.noreply.github.com>
2023-02-08 22:27:52 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -13,7 +13,7 @@ import numpy as np
 
 openai.api_key = st.secrets["API_KEY"]
 
-option = st.sidebar.selectbox("Which Dashboard?", ('Home','Youtube Video Summarizer','Chat Helper Bot Ai','Book Summarizer','AI Translator'),0)
+option = st.sidebar.selectbox("Which Dashboard?", ('Home','Youtube Video Summarizer','Chat Helper Bot Ai','Book Summarizer'),0)
 if option == 'Home':
     st.header(option)
     st.write('The current home for all things AI.')
@@ -83,7 +83,7 @@ if option == 'Book Summarizer':
         transcript = input
         st.write (transcript)
         words = transcript.split(" ")
-        chunks = np.array_split(words, 3)
+        chunks = np.array_split(words, 4)
         sentences = ' '.join(list(chunks[0]))
         summary_responses = []
         for chunk in chunks:
