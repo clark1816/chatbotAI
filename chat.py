9c67518019e4ffa17f2f9d85fe80f0713b647694
clark1816 <33646776+clark1816@users.noreply.github.com>
2023-02-14 09:45:07 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -97,7 +97,7 @@ if option == 'Book Summarizer':
         transcript = input
         st.write (transcript)
         words = transcript.split(" ")
-        chunks = np.array_split(words, 3)
+        chunks = np.array_split(words, 6)
         sentences = ' '.join(list(chunks[0]))
         summary_responses = []
         for chunk in chunks:
