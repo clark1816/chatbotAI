1a7e0b122764cc29fb59d8ff6ea206a79365deb1
clark1816 <33646776+clark1816@users.noreply.github.com>
2023-03-14 09:43:27 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -132,7 +132,7 @@ if option == 'Chat Helper Bot Ai':
             return infile.read()
 
 
-    def gpt3_completion(prompt, engine='text-davinci-002', temp=.9, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['AI:', 'USER:']):
+    def gpt3_completion(prompt, engine='gpt-3.5-turbo', temp=.9, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['AI:', 'USER:']):
         prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
         response = openai.Completion.create(
             engine=engine,
